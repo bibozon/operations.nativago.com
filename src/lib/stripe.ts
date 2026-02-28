0e0816aed0608c2659e6a1b09f4ac5e653f3a18e
Carlos Bozon <bibozon@gmail.com>
2026-02-27 23:52:59 -0500

    fix(stripe): set valid apiVersion for Stripe TS type

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -6,6 +6,6 @@ const STRIPE_SECRET_KEY = getEnv("STRIPE_SECRET_KEY", "");
 export function getStripe(): Stripe | null {
   if (!STRIPE_SECRET_KEY) return null;
   return new Stripe(STRIPE_SECRET_KEY, {
-    apiVersion: "2026-02-25.clover"
+    apiVersion: "2022-11-15"
   });
 }
